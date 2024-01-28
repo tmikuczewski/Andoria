048334414e3829b2d72a3c07528a59679795665e
Tomasz Mikuczewski <miktomi15@gmail.com>
2024-01-28 15:11:20 +0100

    - removed deprications and bump up package

--- Andoria/automotive/build.gradle.kts
+++ Andoria/automotive/build.gradle.kts
@@ -36,10 +36,8 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-
     implementation("androidx.car.app:app:1.2.0")
     implementation("androidx.car.app:app-projected:1.2.0")
 
